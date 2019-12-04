c20cd0d4330d58a09c7f4048bbbc23a45ee91e58
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-12-05 00:04:31 +0100

    improvement: less output in measure_all

--- measure_all.zsh
+++ measure_all.zsh
@@ -1,11 +1,9 @@
 #!/usr/bin/env zsh
 
-set -xeuo pipefail
-
 for f (**/*.rs); do
     rustc -C 'opt-level=3' $f -o ${f:r} &
 done
 
 wait
 
-time ( for f (**/solution); $f )
+time ( for f (**/solution); $f &> /dev/null )
