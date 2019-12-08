f21c15259a08a19a413d79626ab10ef144c06c65
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-12-08 18:42:45 +0100

    improvement(02): optimize by using a binary search

--- 02/solution.rs
+++ 02/solution.rs
@@ -1,9 +1,11 @@
 include!("../intcode.rs");
 
+const TARGET: isize = 19690720;
+
 fn main() {
     let program = load_program(include_str!("input.txt"));
 
-    let run = |noun, verb| {
+    let mut run = |noun, verb| {
         let mut interpreter = Interpreter::new(program.clone());
         interpreter.program[1] = noun;
         interpreter.program[2] = verb;
@@ -13,10 +15,17 @@ fn main() {
 
     println!("{}", run(Cell::from(12), Cell::from(2)));
 
-    let (noun, verb) = (0..100)
-        .flat_map(|noun| (0..100).map(move |verb| (Cell::from(noun), Cell::from(verb))))
-        .find(|(noun, verb)| run(*noun, *verb) == 19690720)
-        .unwrap();
+    let mut options = Vec::with_capacity(100 * 100);
+    for noun in (0..100).map(Cell::from) {
+        for verb in (0..100).map(Cell::from) {
+            options.push((noun, verb));
+        }
+    }
 
-    println!("{}", isize::from(noun) * 100 + isize::from(verb));
+    println!(
+        "{}",
+        options
+            .binary_search_by_key(&TARGET, |(noun, verb)| run(*noun, *verb))
+            .unwrap()
+    );
 }
