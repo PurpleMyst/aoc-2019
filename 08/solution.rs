fb1a0437cf2de63a51a84374bf8b83e57fe7f684
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-12-08 14:20:06 +0100

    feat(08): part2

--- 08/solution.rs
+++ 08/solution.rs
@@ -3,6 +3,10 @@ use std::collections::BTreeMap;
 const WIDTH: usize = 25;
 const HEIGHT: usize = 6;
 
+const BLACK: u8 = b'0';
+const WHITE: u8 = b'1';
+const TRANSPARENT: u8 = b'2';
+
 fn count(arr: &[u8]) -> BTreeMap<u8, usize> {
     let mut result = BTreeMap::new();
 
@@ -20,9 +24,33 @@ fn main() {
         .chunks(WIDTH * HEIGHT);
 
     let layer = layers
+        .clone()
         .map(count)
         .min_by_key(|layer| *layer.get(&b'0').unwrap())
         .unwrap();
 
-    println!("{}", layer.get(&b'1').unwrap() * layer.get(&b'2').unwrap())
+    let mut image = [TRANSPARENT; WIDTH * HEIGHT];
+
+    layers.for_each(|layer| {
+        layer.iter().copied().enumerate().for_each(|(i, pixel)| {
+            if image[i] == TRANSPARENT {
+                image[i] = pixel;
+            }
+        })
+    });
+
+    image.chunks(WIDTH).for_each(|row| {
+        row.iter().copied().for_each(|col| {
+            print!(
+                "{}",
+                match col {
+                    BLACK => ' ',
+                    WHITE => '#',
+                    _ => unreachable!(),
+                }
+            );
+        });
+
+        println!();
+    })
 }
